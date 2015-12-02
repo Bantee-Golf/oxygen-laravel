fdcfe8a372179e5e8bb11590543cf3e7e1a88930
Shane Perera <shane@shaneperera.com>
2015-12-02 21:19:13 +1100

    Resolve Tenant from IoC

--- src/Entities/Auth/TenantRepository.php
+++ src/Entities/Auth/TenantRepository.php
@@ -3,14 +3,22 @@
 namespace EMedia\Oxygen\Entities\Auth;
 
 use EMedia\Oxygen\Entities\BaseRepository;
-use Illuminate\Support\Facades\App;
 
 class TenantRepository extends BaseRepository
 {
 
+	protected $model;
+
+	public function __construct()
+	{
+		$model = app(config('multiTenant.tenantModel'));
+		parent::__construct($model);
+		$this->model = $model;
+	}
+
 	public function getUserByTenant($userId, $tenantId)
 	{
-		$userModel = App::make(config('auth.model'));
+		$userModel = app(config('auth.model'));
 		$query = $userModel::where('id', $userId)
 					 ->whereHas('roles', function ($q) use ($tenantId) {
 					 	$q->where('tenant_id', $tenantId);
