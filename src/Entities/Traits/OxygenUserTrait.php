f1598afb0f136baf6602ab4f1f643e9f612d343e
Shane Perera <shane@shaneperera.com>
2016-09-15 13:55:32 +1000

    Add the missing function again

--- src/Entities/Traits/OxygenUserTrait.php
+++ src/Entities/Traits/OxygenUserTrait.php
@@ -7,7 +7,26 @@ use Silber\Bouncer\Database\HasRolesAndAbilities;
 trait OxygenUserTrait
 {
 
-	use HasRolesAndAbilities;
+	use HasRolesAndAbilities {
+		HasRolesAndAbilities::isA as bouncerIsA;
+	}
+
+	/**
+	 * Override the Bouncer trait's is function and allow passing in an array of roles
+	 * So you can send $user->isA(['admin', 'owner']) as well as $user->isA('admin', 'owner')
+	 *
+	 * @param $roles
+	 * @return bool|mixed
+	 */
+	public function isA($roles)
+	{
+
+		if (is_array($roles)) {
+			return call_user_func_array([$this, 'bouncerIsA'], $roles);
+		}
+
+		return $this->bouncerIsA($roles);
+	}
 
 	public function hasFirstName()
 	{
