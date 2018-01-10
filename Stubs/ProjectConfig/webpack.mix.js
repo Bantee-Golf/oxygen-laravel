6eeec465981921933e3226f3f3cf255308071847
Shane Perera <shane@shaneperera.com>
2018-01-10 16:55:03 +1100

    Fix missing css

--- Stubs/ProjectConfig/webpack.mix.js
+++ Stubs/ProjectConfig/webpack.mix.js
@@ -14,6 +14,7 @@ let mix = require('laravel-mix');
 mix.setPublicPath('public_html/');
 
 mix.sass('resources/assets/sass/dashboard/dashboard.scss', 'public_html/css/dist')
+	.sass('resources/assets/sass/auth.scss', 'public_html/css/dist')
 	.sass('resources/assets/sass/public.scss', 'public_html/css/dist')
 	.sourceMaps();
 
