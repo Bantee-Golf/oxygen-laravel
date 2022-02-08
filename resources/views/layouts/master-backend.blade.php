df96c18fc62e49b6e6304c3a366e51cae9a63db5
Shane Perera <shane7@gmail.com>
2022-02-09 09:53:20 +1100

    Change dropdown element ID

--- resources/views/layouts/master-backend.blade.php
+++ resources/views/layouts/master-backend.blade.php
@@ -75,11 +75,11 @@
 								</li>
 							@else
 								<li class="nav-item dropdown">
-									<a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown"
+									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
 									   role="button" data-bs-toggle="dropdown" aria-expanded="false">
 										{{ Auth::user()->name }}
 									</a>
-									<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+									<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
 										<li>
 											<a class="dropdown-item" href="{{ route('account.profile') }}">
 												{{ __('My Profile') }}
